885a698e46aa90d2c8e9f2880b0283272f3d910e
Nguyen Khang <huukhangvn@gmail.com>
2021-02-05 15:35:03 +0700

    ok

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
-# main.py
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, requests
+from fastapi.responses import FileResponse, RedirectResponse
+import cv2
 app = FastAPI()
 
+
 @app.get("/")
-def hello():
-    return {"message":"Hello TutLinks.com"}
+async def main():
+    return {"Hello": "World!"}
+
+def process_video(filename):
+    cap = cv2.VideoCapture(filename)
+    fourcc = cv2.VideoWriter_fourcc('m','p','4','v')
+    out = cv2.VideoWriter('output.mp4',fourcc, 20.0, (1280,720))
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
+            # write the flipped frame
+            out.write(frame)
+        else:
+            break
+
+    # Release everything if job is finished
+    cap.release()
+    out.release()
+
+@app.post("/video_upload")
+async def upload_video(file: UploadFile = File(...)):
+    extension = file.filename.split(".")[-1] in ("mp4", "wav")
+    if not extension:
+        return "Video must in mp4 or wav format!"
+    process_video(file.filename)
+    return FileResponse("output.mp4", media_type="video/mp4")
+
+    
